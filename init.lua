e53537f99cb1864a69b3255b90aa7aac6b854980
madeem-delvi <francescapham5055@gmail.com>
2024-11-30 13:10:50 -0500

    updated neotree behavior

--- init.lua
+++ init.lua
@@ -15,9 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
---[[
---	Usage: { import = "plugins.treesitter" },
---]]
+-- Usage: { import = "plugins.treesitter" },
 require("lazy").setup({
   spec = {
 	{ import = "plugins.colorscheme"},
--- lua/settings.lua
+++ lua/settings.lua
@@ -5,3 +5,6 @@ vim.g.maplocalleader = "\\"
 -- Enable line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Force it to share clipboard with system
+vim.opt.clipboard = "unnamedplus"
