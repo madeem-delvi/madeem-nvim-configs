25c68f4802f2d3f036b0bcf8e129cde607b10796
madeem-delvi <francescapham5055@gmail.com>
2024-12-10 12:14:27 -0500

    removed folding behavior (too buggy. will fix later)

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,9 +6,5 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.o.statuscolumn = "%s %l %r"
 
--- Set fold behavior
-vim.opt.foldmethod = 'indent'
-vim.opt.foldtext = 'getline(vim.foldstart) .. " [...] " .. (vim.foldend - vim.foldstart + 1) .. " lines hidden"'
-
 -- Force it to share clipboard with system
 vim.opt.clipboard = "unnamedplus"
