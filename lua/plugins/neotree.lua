36d333858cb39729957da80c8f3fe4fc0ff70c5b
madeem-delvi <francescapham5055@gmail.com>
2024-12-17 00:14:08 -0500

    switched to float menu

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,18 +6,6 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		-- Allows dynamic use of <C-n> based on context
-		vim.keymap.set("n", "<C-n>", function()
-			local neotree_is_focused = vim.fn.win_getid() == vim.fn.bufwinid("neo-tree")
-			-- Behaves as a normal toggle when panel is focued
-			if neotree_is_focused then
-				vim.cmd(":Neotree toggle")
-			-- If it is not focus (ex. while cursor is in text editor) will just focus instead
-			else
-				vim.cmd(":Neotree focus")
-			end
-		end)
-
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree reveal float<CR>", {})
 	end,
 }
